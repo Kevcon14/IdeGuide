23a83c8d1148213bff8a4c0b926f96ed5a2e2002
Kevin <Kevin@10.213.158.72>
2020-01-27 20:10:16 -0500

    new commit

---
+++ src/main/java/IdeGuide.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Kevin    
+ */
+public class IdeGuide {
+    public static void main(String args[]) {
+        int i = 15;
+        while(i <25){
+            System.out.println("Not there yet!");
+            i++;
+        }
+        System.out.println("There.");
+    }
+}
